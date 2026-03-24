c417d0bc7ff9e066f7b3f296c9de646c5c62c70a
Rogerium2102 <MatthewR2110@outlook.com>
2026-03-24 10:16:58 +0000

    Added tests for 5, 0, 1, 42, -1 and added check for negatives

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -5,6 +5,8 @@ def factorial(n):
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
+    if n < 0:
+        raise ValueError("n cannot be negative")
     factorial = 1
     for i in range(1, n + 1):
         factorial = factorial * i
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,6 +1,21 @@
 import pytest
+import math
 from mymath.factorial import factorial
-
 
 def test_3():
     assert factorial(3) == 6
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_0():
+    assert factorial(0) == math.factorial(0)
+
+def test_1():
+    assert factorial(1) == 1
+
+def test_42():
+    assert factorial(42) == math.factorial(42)
+
+def test_negative1():
+    pytest.raises(ValueError, factorial, -1)
